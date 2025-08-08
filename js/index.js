071eb8ac71261c427b71af6b092e26600a406279
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-08-08 19:31:22 +0200

    added index.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById("bookmarkBtn").addEventListener("click", function () {
+  // Change the image to the filled version
+  this.src = "./assets/bookmark.png";
+
+  window.location.href = "anotherpage.html";
+});
